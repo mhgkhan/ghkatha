25a3e7c6f51249e634753ced4f4b2cef6c9bd472
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2023-12-26 09:50:59 +0500

    HEADER & TOGGLE, RESPONSIVE NAVBAR COMPLETED

--- src/components/LandingNavbar.jsx
+++ src/components/LandingNavbar.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FaAlignJustify } from 'react-icons/fa'
 import { GrClose } from 'react-icons/gr'
 
 const LandingNavbar = () => {
+
+  const [innerWidth, setinnerWidth] = useState(null)
+  const [navOpen, setNavopen] = useState(false)
+
+
+  useEffect(() => {
+    if (window.innerWidth > 600) setinnerWidth(null)
+    else setinnerWidth(true)
+  }, [])
+
+
+
+
+
   return (
     <header>
-      <div className="logo">
-        <img src="/logo.png" alt="this website logo" />
-      </div>
-      <div className="button-toggle-navbar"><FaAlignJustify /></div>
-      <nav>
-        <ul>
-          <li><Link to="/">HOME</Link></li>
-          <li><Link to="/signup">SIGNUP</Link></li>
-          <li><Link to="/about">ABOUT</Link></li>
-          <li><Link to="/contact">CONTACT</Link></li>
-          <li><a href="https://github.com/mhgkhan/ghkatha">GITHUB</a></li>
-        </ul>
-      </nav>
+      <div className="container">
 
+        <div className="nav-top">
+          <div className="logo">
+            <img src="/logo.png" alt="this website logo" />
+          </div>
+          {
+            innerWidth || innerWidth !== null ? <div className="button-toggle-navbar" onClick={() => setNavopen(!navOpen)}>{!navOpen ? <FaAlignJustify /> : <GrClose />}</div> : null
+          }
+        </div>
+
+        <nav style={{height:innerWidth || innerWidth !== null ? navOpen ? "60vh" : "0px" : "auto"}}>
+          <ul>
+            <li><Link to="/">HOME</Link></li>
+            <li><Link to="/signup">SIGNUP</Link></li>
+            <li><Link to="/about">ABOUT</Link></li>
+            <li><Link to="/contact">CONTACT</Link></li>
+            <li><a href="https://github.com/mhgkhan/ghkatha">GITHUB</a></li>
+          </ul>
+        </nav>
+
+
+      </div>
     </header>
   )
 }
