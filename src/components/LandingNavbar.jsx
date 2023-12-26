dfc6a9ee1421d26c5b80a6b5eeb8a9acaeaf7630
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2023-12-26 10:27:20 +0500

    ADDED SOME STYLING TO NAV MENU

--- src/components/LandingNavbar.jsx
+++ src/components/LandingNavbar.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { FaAlignJustify } from 'react-icons/fa'
 import { GrClose } from 'react-icons/gr'
 
+
 const LandingNavbar = () => {
+  
+  
+  
+  const location = useLocation();
 
   const [innerWidth, setinnerWidth] = useState(null)
   const [navOpen, setNavopen] = useState(false)
@@ -12,13 +17,14 @@ const LandingNavbar = () => {
   useEffect(() => {
     if (window.innerWidth > 600) setinnerWidth(null)
     else setinnerWidth(true)
-  }, [])
+  }, [location.pathname])
 
 
 
 
 
   return (
+
     <header>
       <div className="container">
 
@@ -31,12 +37,12 @@ const LandingNavbar = () => {
           }
         </div>
 
-        <nav style={{height:innerWidth || innerWidth !== null ? navOpen ? "60vh" : "0px" : "auto"}}>
+        <nav style={{ height: innerWidth || innerWidth !== null ? navOpen ? "60vh" : "0px" : "auto" }}>
           <ul>
-            <li><Link to="/">HOME</Link></li>
-            <li><Link to="/signup">SIGNUP</Link></li>
-            <li><Link to="/about">ABOUT</Link></li>
-            <li><Link to="/contact">CONTACT</Link></li>
+            <li><Link to="/"  style={{color:location.pathname === "/" ? "white":"black", background:location.pathname === "/"?"violet":"none"}}>HOME</Link></li>
+            <li><Link to="/signup"  style={{color:location.pathname === "/signup" ? "white":"black", background:location.pathname === "/signup"?"violet":"none"}} >SIGNUP</Link></li>
+            <li><Link to="/about"  style={{color:location.pathname === "/about" ? "white":"black", background:location.pathname === "/about"?"violet":"none"}} >ABOUT</Link></li>
+            <li><Link to="/contact"  style={{color:location.pathname === "/contact" ? "white":"black", background:location.pathname === "/contact"?"violet":"none"}} >CONTACT</Link></li>
             <li><a href="https://github.com/mhgkhan/ghkatha">GITHUB</a></li>
           </ul>
         </nav>
