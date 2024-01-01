23ae3d99a891fb18145fb1ac0925527a8f039004
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2024-01-01 10:08:47 +0500

    KATHA PAGE , CREATE KATHA UI COMPLETED

---
+++ src/components/dailogs/CreateKathaBox.jsx
@@ -0,0 +1,124 @@
+import React, { useRef } from 'react'
+import Inputs from '../form/Inputs'
+import { GrClose } from 'react-icons/gr'
+import { GrEmptyCircle } from 'react-icons/gr'
+
+const CreateKathaBox = ({ formRef,loading, submitForm, changeInputs, formInputs, setImg1Val, setImg2Val, openedCreateKatha, openKathaCreateBox, setPic1Path, setPic2Path, pic1Path, pic2Path }) => {
+
+    const ref1 = useRef()
+    const ref2 = useRef()
+
+    const changePic1 = (e) => {
+        console.log(e.target)
+        if (e.target.files.length > 0) {
+            const localPath = URL.createObjectURL(e.target.files[0])
+            setImg1Val(e.target.value)
+            setPic1Path(localPath)
+        }
+        else {
+            return alert("Please select an image");
+        }
+    }
+    const changePic2 = (e) => {
+        console.log(e.target)
+        if (e.target.files.length > 0) {
+            const localPath = URL.createObjectURL(e.target.files[0])
+            setImg2Val(e.target.value)
+            setPic2Path(localPath)
+        }
+        else {
+            return alert("Please select an image");
+        }
+    }
+
+
+
+    return (
+        <div className="katha-dailog" style={{ transform: `translateY(${openedCreateKatha ? "0px" : "-2000vh"})` }} >
+            <div className="close-create-katha" onClick={openKathaCreateBox}><GrClose /></div>
+
+            <div className="container">
+
+                <div className="katha-container">
+                    <form ref={formRef} onSubmit={submitForm}>
+                        <h1>Create a new katha </h1>
+                        <p>Please Complete this form to create new secure and trusted person katha </p>
+
+                        <br /><br />
+                        <h3>Upload </h3>
+                        <p>upload the user image and scanned cnic front picture with the resolution of 300 dpi </p>
+
+                        <div className="upload-area-container">
+
+                            <div className="picture-area upload-block">
+                                <img src={pic1Path.length < 0 ? "/author-1.png" : pic1Path} alt="upload" />
+                                <div className="upload-input upload-input-pic">
+                                    <input ref={ref1} type="file" onChange={changePic1} accept='image/*' />
+                                    <h2 style={{ color: "white", textAlign: "center" }}>Upload Image</h2>
+                                    <br />
+                                    <button type='button' onClick={() => ref1.current.click()} className='uploadBtn'>Upload</button>
+                                </div>
+                            </div>
+
+                            <div className="picture-area upload-block">
+                                <img src={pic2Path.length < 0 ? "/author-1.png" : pic2Path} alt="upload" />
+                                <div className="upload-input upload-input-pic">
+                                    <input ref={ref2} type="file" onChange={changePic2} accept='image/*' />
+                                    <h2 style={{ color: "white", textAlign: "center" }}>Upload Cnic</h2>
+                                    <br />
+                                    <button type='button' onClick={() => ref2.current.click()} className='uploadBtn'>Upload</button>
+                                </div>
+                            </div>
+
+
+
+
+                        </div>
+
+
+                        <div className="form-inputs-container">
+                            <h3>Personal Information  </h3>
+                            <p>upload the user image and scanned cnic front picture with the resolution of 300 dpi </p>
+                            <div className="parent-inputs">
+                                <div className="fullname child-input">
+                                    <Inputs name={'fullname'} type={'text'} ph={'Full Name'} required={true} minl={3} onchange={changeInputs} val={formInputs.name} />
+                                </div>
+                                <div className="country child-input">
+                                    <Inputs type={'text'} name={'father'} required={true} ph={'Father name'} minl={'2'} onchange={changeInputs} val={formInputs.father} />
+                                </div>
+                                <div className="country child-input">
+                                    <Inputs type={'text'} name={'cnic'} required={true} ph={'Enter Cnic'} minl={'10'} onchange={changeInputs} val={formInputs.cnic} />
+                                </div>
+                                <div className="country child-input">
+                                    <Inputs type={'text'} name={'phone'} required={true} ph={'Phone Number'} minl={'2'} onchange={changeInputs} val={formInputs.phone} />
+                                </div>
+                                <div className="country child-input">
+                                    <Inputs type={'text'} name={'area'} required={true} ph={'Area'} minl={'2'} onchange={changeInputs} val={formInputs.area} />
+                                </div>
+                            </div>
+                            <div className="parent-inputs">
+                                <textarea name="address" required cols="30" rows="7" placeholder='Enter full address ' onChange={changeInputs} value={formInputs.address} ></textarea>
+                            </div>
+
+                        </div>
+
+                        <button className='form-createkatha-button resetBtn' type='reset' >Clear Form </button>
+                        <button className='form-createkatha-button' type='submit' >{loading? <GrEmptyCircle /> : "Create Now"}</button>
+
+
+                    </form>
+                </div>
+
+
+
+            </div>
+
+
+
+
+        </div>
+
+    )
+}
+
+export default CreateKathaBox
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Inputs from '../components/form/Inputs'
+import CreateKathaBox from '../components/dailogs/CreateKathaBox';
+
 
 const rawData = [
   { name: "ghazna", cnic: "21202-1512793-5", area: "kass Killy" },
@@ -11,13 +13,29 @@ const rawData = [
   { name: "Fazal Amin", cnic: "212032-1512732-5", area: "gudar" },
 ]
 
+
+
 const Home = () => {
 
   const nav = useNavigate();
+  const kathaRef = useRef();
+
 
   const [authToken, setAuthToken] = useState("")
   const [rawDataset, setRawData] = useState(rawData);
+  const [openedCreateKatha, setOpenCreateKatha] = useState(false)
 
+  const [pic1Path, setPic1Path] = useState("/author-1.png")
+  const [pic2Path, setPic2Path] = useState("/author-1.png")
+
+
+  const [img1Val, setImg1Val] = useState("");
+  const [img2Val, setImg2val] = useState("")
+
+
+  const [loading, setLoading] = useState(false)
+
+  const [oldatabtn, setOlddatabtn] = useState(false)
 
 
   const checkAuthorize = () => {
@@ -30,19 +48,23 @@ const Home = () => {
 
   const changeSearch = e => {
     setSearchVal(e.target.value)
-    setRawData(rawData.filter(katha => katha.name === serchVal))
+    if (serchVal.length < 2 ? setRawData(rawData) : setRawData(rawData.filter((katha, index) => katha.name.toLowerCase().split("")[index] === serchVal.toLowerCase().split("")[index])))
+      setSearchVal("")
   }
 
 
   const filterKathasBySelection = e => {
-    setRawData(rawData.filter((katha, index) => katha.area === e.target.value))
+    setRawData(rawDataset.filter((katha, index) => katha.area === e.target.value))
   }
 
-  const filterByDateNew = () => {
+  const resetAllData = () => {
+    // const oldRaw = rawDataset
     setRawData(rawData)
   }
-  const filterByDateOld = () => {
-    setRawData(rawData.reverse())
+  const sortByDate = () => {
+    const newRawData = rawDataset
+    setRawData([...newRawData.reverse()]);
+    setOlddatabtn(!oldatabtn)
   }
 
 
@@ -51,13 +73,59 @@ const Home = () => {
     checkAuthorize();
   })
 
+
+  const openKathaCreateBox = () => {
+    setOpenCreateKatha(!openedCreateKatha)
+  }
+
+  const [formInputs, setFormInputs] = useState({
+    fullname: "",
+    father: "",
+    cnic: "",
+    phone: "",
+    area: "",
+    address: ""
+  })
+
+  const changeInputs = (e) => {
+    setFormInputs({ ...formInputs, [e.target.name]: e.target.value })
+  }
+
+  const submitCreateKathaForm = (e) => {
+    e.preventDefault();
+    setLoading(true)
+    const formData = new FormData()
+    formData.append("fullname", formInputs.fullname)
+    formData.append("father", formInputs.father)
+    formData.append("cnic", formInputs.cnic)
+    formData.append("phone", formInputs.phone)
+    formData.append("area", formInputs.area)
+    formData.append("address", formInputs.address)
+    formData.append("userImg", img1Val)
+    formData.append("cnicImg", img2Val)
+
+
+    const AddtoRawData = { name: formInputs.fullname, cnic: formInputs.cnic, area: formInputs.area }
+    rawData.concat(AddtoRawData);
+    setRawData([...rawDataset, AddtoRawData]);
+    setTimeout(() => {
+      setLoading(false);
+      kathaRef.current.reset()
+      setOpenCreateKatha(false);
+    }, 1000);
+  }
+
+
   return (
-    <main>
+    <main className='main-katha-main'>
+
+      <CreateKathaBox loading={loading} formRef={kathaRef} submitForm={submitCreateKathaForm} changeInputs={changeInputs} formInputs={formInputs} setImg1Val={setImg1Val} setImg2Val={setImg2val} openedCreateKatha={openedCreateKatha} openKathaCreateBox={openKathaCreateBox} setPic1Path={setPic1Path} setPic2Path={setPic2Path} pic1Path={pic1Path} pic2Path={pic2Path} />
+
       <section className='main-header'>
         <div className="container">
           <div className="username"><h1>{authToken}</h1></div>
           <div className="actions-katha">
-            <button>Create new </button>
+            <button onClick={openKathaCreateBox}>Create new </button>
           </div>
         </div>
       </section>
@@ -86,8 +154,8 @@ const Home = () => {
               <span>Filter by area</span> <br />
               <select name="filter-by-add" onChange={filterKathasBySelection}>
                 <option defaultChecked >Select Area </option>
-                {rawData.map(katha => {
-                  return <option value={katha.area}>{katha.area.toUpperCase()}</option>
+                {rawDataset.map((katha, index) => {
+                  return <option key={index} value={katha.area}>{katha.area.toUpperCase()}</option>
                 })}
 
               </select>
@@ -96,8 +164,8 @@ const Home = () => {
             <div className="by-date">
               <div className="header-bydate"><span>Filter By Date </span></div>
               <div className="buttons-filter">
-                <button onClick={filterByDateNew}>Newest </button>
-                <button onClick={filterByDateOld}>Oldest</button>
+                <button onClick={resetAllData}>Reset </button>
+                <button onClick={sortByDate}>{oldatabtn ? "Newist" : "Oldest"}</button>
               </div>
             </div>
 
@@ -124,7 +192,7 @@ const Home = () => {
               <p>Open</p>
             </div>
             {
-              rawDataset.map((katha, index) => <div className=" katha-block">
+              rawDataset.map((katha, index) => <div className=" katha-block" key={index}>
                 <p className='serial-no'>{index + 1}</p>
                 <p className="fullname-k-block">{katha.name}</p>
                 <p className='cnic-k-block'>{katha.cnic}</p>
